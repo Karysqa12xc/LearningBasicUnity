1ade22ecd6ba94e211dff54a781fad3e705eb16e
Karysqa <85331340+Karysqa12xc@users.noreply.github.com>
2024-01-10 16:52:17 +0700

    feat: finished chapter2

--- Chapter1+2/Hero Born/Assets/Scripts/LearningCurve.cs
+++ Chapter1+2/Hero Born/Assets/Scripts/LearningCurve.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
+    public int CurrentAge = 30;
+    public int AddAge = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ComputeAge();
     }
-
+    /// <summary>
+    /// Time to action - adding comments
+    /// Computer a modified age integer
+    /// </summary>
+    void ComputeAge(){
+        Debug.Log(CurrentAge + AddAge);
+    }
     // Update is called once per frame
     void Update()
     {
--- Readme.md
+++ Readme.md
@@ -6,3 +6,8 @@ particular Unity component or feature. Where can you find more detailed
 3. The Scripting Reference is a large document. How much of it do you have to 
 memorize before attempting to write a script?
 4. When is the best time to name a C# script?
+# Pop quiz – C# building blocks(Chap 2)
+1. What is the main purpose of a variable?
+2. What role do methods play in scripts?
+3. How does a script become a component?
+4. What is the purpose of dot notation?
